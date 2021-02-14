740b4ec5d7f8c1e24b1c2b8bec35eba97675b184
Dayana <dayana.toledo@unesp.br>
2021-02-14 01:53:23 -0300

    feat: add data json

---
+++ data/data.js
@@ -0,0 +1,54 @@
+let products = [
+    {
+        "name": "Geladeira Brastemp",
+        "category": 0,
+        "price": 2500.50,
+        "image": "geladeira.webp"
+    },
+    {
+        "name": "Geladeira Brastemp",
+        "category": 0,
+        "price": 2500.50,
+        "image": "geladeira.webp"
+    },
+    {
+        "name": "Geladeira Brastemp",
+        "category": 0,
+        "price": 2500.50,
+        "image": "geladeira.webp"
+    },
+    {
+        "name": "Geladeira Brastemp",
+        "category": 0,
+        "price": 2500.50,
+        "image": "geladeira.webp"
+    },
+    {
+        "name": "Geladeira Brastemp",
+        "category": 0,
+        "price": 2500.50,
+        "image": "geladeira.webp"
+    },
+    {
+        "name": "Geladeira Brastemp",
+        "category": 0,
+        "price": 2500.50,
+        "image": "geladeira.webp"
+    }
+];
+
+let category = [
+    "Todos",
+    "Eletrônicos",
+    "Informática",
+    "Roupas",
+    "Calçados",
+    "Papelaria",
+    "Movéis",
+    "Casa, mesa e banho",
+    "Eletroportáteis",
+    "Brinquedos",
+    "Esportes e lazer",
+    "Ferramentas e jardim",
+    "Pet shop"
+]
