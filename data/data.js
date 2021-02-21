c0d2f05322f581f29dded23505bbc473edd1b1b6
Dayana <dayana.toledo@unesp.br>
2021-02-20 22:12:45 -0300

    feat: add new products data.js

--- data/data.js
+++ data/data.js
@@ -1,13 +1,13 @@
 let products = [
     {
         "name": "Geladeira Brastemp",
-        "category": 3,
+        "category": 1,
         "price": 3500.50,
         "image": "geladeira.webp"
     },
     {
         "name": "Caderno Tilibra",
-        "category": 2,
+        "category": 5,
         "price": 29.99,
         "image": "caderno.jpg"
     },
@@ -37,27 +37,97 @@ let products = [
     },
     {
         "name": "Tênis Nike",
-        "category": 1,
+        "category": 2,
         "price": 1497.59,
         "image": "tenis.jpg"
     },
     {
         "name": "Toalha Colorida",
-        "category": 4,
+        "category": 3,
         "price": 49.69,
         "image": "toalha.jpg"
+    },
+    {
+        "name": "Jogo de Lençol",
+        "category": 3,
+        "price": 350.59,
+        "image": "lencol.jpg"
+    },
+    {
+        "name": "Roupa Infantil",
+        "category": 2,
+        "price": 198.88,
+        "image": "infantil.jpeg"
+    },
+    {
+        "name": "Lápis de Cor",
+        "category": 5,
+        "price": 311.22,
+        "image": "lapis.jpg"
+    },
+    {
+        "name": "Ração Royal",
+        "category": 7,
+        "price": 1497.59,
+        "image": "racao.jpg"
+    },
+    {
+        "name": "Mochila Escolar",
+        "category": 5,
+        "price": 70,
+        "image": "mochila.jpg"
+    },
+    {
+        "name": "Moletom HP",
+        "category": 2,
+        "price": 119.90,
+        "image": "moletom.jpg"
+    },
+    {
+        "name": "Cama de Cachorro",
+        "category": 7,
+        "price": 89.90,
+        "image": "cama.jpeg"
+    },
+    {
+        "name": "Boneca Nelly",
+        "category": 6,
+        "price": 58.90,
+        "image": "boneca.jpg"
+    },
+    {
+        "name": "Micro-ondas Consul",
+        "category": 1,
+        "price": 531.44,
+        "image": "micro.jpg"
+    },
+    {
+        "name": "Bicicleta HUPI",
+        "category": 4,
+        "price": 823,
+        "image": "bike.jpg"
+    },
+    {
+        "name": "Armário de Cozinha",
+        "category": 1,
+        "price": 898.69,
+        "image": "armario.jpg"
+    },
+    {
+        "name": "LEGO City",
+        "category": 6,
+        "price": 279.99,
+        "image": "lego.jpg"
     }
 ];
 
 let category = [
     "Eletrônicos",
+    "Movéis e eletrodomésticos",
     "Roupas e calçados",
-    "Papelaria",
-    "Movéis",
     "Casa, mesa e banho",
-    "Eletroportáteis",
-    "Brinquedos",
     "Esportes e lazer",
-    "Ferramentas e jardim",
+    "Papelaria",
+    "Brinquedos",
     "Pet shop"
 ]
